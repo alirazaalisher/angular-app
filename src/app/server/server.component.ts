ba2a40d4fbbacf3a1203b8baa599616c0954402f
Ali Raza <ali.raza@khired.com>
2020-11-25 00:00:52 +0500

    data binding string interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.scss']
+  styleUrls: ['./server.component.scss'],
 })
 export class ServerComponent implements OnInit {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  getServerStatus() {
+    return this.serverStatus;
   }
-
 }
